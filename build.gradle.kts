f9ff84614527136530b7a750a07b8fe77d013156
marvinbuff <marvinbuff@hotmail.com>
2019-07-24 11:43:58 +0200

    Configured gradle build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,3 @@
-//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-//
-//plugins {
-//  kotlin("jvm") version "1.3.40"
-//  id("me.champeau.gradle.jmh") version "0.4.8"
-//  id("com.github.ben-manes.versions") version "0.21.0"
-//  id("io.morethan.jmhreport") version "0.9.0"
-//}
-//
-//group  = ""
-//version = "SNAPSHOT"
-//
-//dependencies {
-//  implementation(kotlin("stdlib"))
-//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-M1")
-//  testImplementation("junit:junit:4.12")
-//
-//  "jmhImplementation"("org.openjdk.jmh:jmh-core:1.21")
-//  "jmhImplementation"("org.openjdk.jmh:jmh-generator-annprocess:1.21")
-//  "jmhImplementation"("commons-io:commons-io:2.4")
-//}
-//
-//jmh {
-//  include = listOf("javaFxGame\\.ActorBenchmark.*")
-//  fork = 2
-//  warmupIterations = 1
-//  iterations = 1
-//  jvmArgs = listOf("-Djmh.separateClasspathJAR=true")
-//  resultFormat = "JSON"
-//}
-//
-//sourceSets {
-//  jmh
-//}
-//
-//repositories {
-//  mavenCentral()
-//  jcenter()
-//}
-//
-//tasks {
-//  withType<KotlinCompile> {
-//
-//    kotlinOptions {
-//      jvmTarget = "1.8"
-//
-//      freeCompilerArgs = freeCompilerArgs + listOf(
-//        "-Xuse-experimental=kotlin.Experimental"
-//      )
-//    }
-//  }
-//}
-//
-//tasks.create<Zip>("zip") {
-//  description = "Archives sources in a zip file"
-//  group = "Archive"
-//
-//  from("src")
-//  archiveName = "basic-demo-1.0.zip"
-//}
-
 import io.morethan.jmhreport.gradle.task.JmhReportTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -77,14 +16,6 @@ repositories {
   jcenter()
 }
 
-jmh {
-  fork = 2
-  jvmArgs = listOf("-Djmh.separateClasspathJAR=true")
-  resultFormat = "JSON"
-
-//     include = listOf("PersistentListBenchmark") // <-- Use regex
-}
-
 dependencies {
   api(kotlin("stdlib"))
   api("it.unimi.dsi:fastutil:8.2.3")
@@ -94,6 +25,12 @@ dependencies {
 
   "jmhImplementation"("org.openjdk.jmh:jmh-core:1.21")
   "jmhImplementation"("org.openjdk.jmh:jmh-generator-annprocess:1.21")
+}
+
+jmh {
+  fork = 2
+  jvmArgs = listOf("-Djmh.separateClasspathJAR=true")
+  resultFormat = "JSON"
 }
 
 jmhReport {
@@ -109,7 +46,18 @@ tasks {
   withType<KotlinCompile> {
     kotlinOptions {
       jvmTarget = "1.8"
-      freeCompilerArgs += listOf("-XXLanguage:+InlineClasses")
+
+      freeCompilerArgs = freeCompilerArgs + listOf(
+        "-Xuse-experimental=kotlin.Experimental"
+      )
     }
   }
+}
+
+tasks.create<Zip>("zip") {
+  description = "Archives sources in a zip file"
+  group = "Archive"
+
+  from("src")
+  archiveName = "basic-demo-1.0.zip"
 }
--- src/jmh/kotlin/javaFxGame/ActorBenchmark.kt
+++ src/jmh/kotlin/javaFxGame/ActorBenchmark.kt
@@ -1,8 +1,6 @@
 package javaFxGame
 
 import org.openjdk.jmh.annotations.*
-import org.openjdk.jmh.runner.Runner
-import org.openjdk.jmh.runner.options.OptionsBuilder
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -14,16 +12,7 @@ open class ActorBenchmark {
 
     @Benchmark
     fun deleteActorsByDeathMark() {
-        val sum = (0..2).map { Random.nextInt() }.sum()
+        val sum = (0..1).map { Random.nextInt() }.sum()
         println("Sum is $sum")
     }
-}
-
-fun main() {
-    val opt = OptionsBuilder()
-        .include(".*" + ActorBenchmark::class.java.simpleName + ".*")
-        .forks(1)
-        .build()
-
-    Runner(opt).run()
 }
