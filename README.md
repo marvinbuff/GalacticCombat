407c1bdf9d57edbbdcdad0985ed97b30d8437912
marvinbuff <marvinbuff@hotmail.com>
2019-09-18 17:05:00 +0200

    Adjusted gradle files for single repo

--- README.md
+++ README.md
@@ -1,49 +1,9 @@
-# KotlinPlayground
-
-This repository contains several Kotlin related projects.
-
-___
-### Galactic Combat
+# Galactic Combat
 
 A tower defense game using FXGL library.
-- package: `galacticCombat`
-- Main: `galacticCombat.GalacticCombatApp`
 
 Resources
 - [FXGL Library](https://github.com/AlmasB/FXGL)
 - [FXGL Wiki](https://github.com/AlmasB/FXGL/wiki/FXGL-11)
 - [Examples](https://github.com/AlmasB/FXGLGames)
 - [Example - Tower Defense](https://github.com/AlmasB/FXGLGames/tree/master/TowerDefense)
-
-___
-### Space Wars
-
-A small game to learn JavaFX and Kotlin.
-- package: `javaFxGame`
-- Main: `javaFxGame.main`
-
-___
-### Benchmarking
-
-Small example to try out benchmarking with JMH.
-- Run: `gradlew jmh`
-- Create Report: `gradlew jmhReport`
-- Remove Daemons: `gradlew --stop`
-
-Resources
-- [JMH Tutorial](http://tutorials.jenkov.com/java-performance/jmh.html)
-
-___
-### Json DSL
-
-A Kotlin-DSL for writing Json with a custom example which writes the entity config json.
-
-___
-### Samples
-
-A collection of very small self-contained code snippets.
-
-- `ArrayListFun.kt`
-- `CoroutineFun.kt`
-- `JavaFxFun.kt`
-- `MergeSort.kt`
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,5 @@
-import io.morethan.jmhreport.gradle.task.JmhReportTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
   kotlin("jvm") version "1.3.50"
-
-  // Benchmarking
-  id("me.champeau.gradle.jmh") version "0.5.0-rc-1"
-  id("com.github.ben-manes.versions") version "0.21.0"
-  id("io.morethan.jmhreport") version "0.9.0"
 
   // json serialization
   id("kotlinx-serialization") version "1.3.50"
@@ -36,8 +28,6 @@ repositories {
 
 dependencies {
   api(kotlin("stdlib"))
-  api("it.unimi.dsi:fastutil:8.2.3")
-  api("org.pcollections:pcollections:3.0.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
   testImplementation("junit:junit:4.12")
 
@@ -47,49 +37,9 @@ dependencies {
 
   // FXGL
   compile("com.github.almasb:fxgl:11.4")
-
-
-  // JMH?
-//  "jmhImplementation"("org.openjdk.jmh:jmh-core:1.21")
-//  "jmhImplementation"("org.openjdk.jmh:jmh-generator-annprocess:1.21")
 }
 
 javafx {
   version = "12.0.2"
   modules = mutableListOf("javafx.controls")
-}
-
-jmh {
-  fork = 2
-  jvmArgs = listOf("-Djmh.separateClasspathJAR=true")
-  resultFormat = "JSON"
-}
-
-jmhReport {
-  jmhResultPath = "build/reports/jmh/results.json"
-  jmhReportOutput = "build/reports/jmh"
-}
-
-tasks {
-  withType<JmhReportTask> {
-    dependsOn("jmh")
-  }
-
-  withType<KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "1.8"
-
-      freeCompilerArgs = freeCompilerArgs + listOf(
-        "-Xuse-experimental=kotlin.Experimental"
-      )
-    }
-  }
-}
-
-tasks.create<Zip>("zip") {
-  description = "Archives sources in a zip file"
-  group = "Archive"
-
-  from("src")
-  archiveName = "basic-demo-1.0.zip"
 }
