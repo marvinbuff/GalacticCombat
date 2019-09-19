c0650dda6f55d28efbc6f19f51e6b82e470c1b83
marvinbuff <marvinbuff@hotmail.com>
2019-09-19 16:14:49 +0200

    Fixed bullet

--- src/main/kotlin/galacticCombat/bullet/BulletFactory.kt
+++ src/main/kotlin/galacticCombat/bullet/BulletFactory.kt
@@ -5,29 +5,25 @@ import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.EntityFactory
 import com.almasb.fxgl.entity.SpawnData
 import com.almasb.fxgl.entity.Spawns
-import com.almasb.fxgl.entity.components.CollidableComponent
 import galacticCombat.AssetsConfig
 import galacticCombat.BULLET_ID
 import galacticCombat.EntityType
-import galacticCombat.invader.HealthComponent
-import galacticCombat.invader.InvaderComponent
 
 @Suppress("unused")
 class BulletFactory : EntityFactory {
 
 
+  /**
+   * Spawns a Bullet.
+   * SpawnData [data] must have a set "target".
+   */
   @Spawns(BULLET_ID)
-  fun spawnEnemy(data: SpawnData): Entity {
-    val invader = InvaderComponent()
-    val healthBar = HealthComponent(invader)
-
+  fun spawnBullet(data: SpawnData): Entity {
     return entityBuilder().type(EntityType.BULLET)
-      .from(data)
-      .viewWithBBox(AssetsConfig.getEnemy("1.1.gif"))
-      .with(CollidableComponent(true))
-      .with(invader)
-      .with(healthBar)
-      .build()
+      .view(AssetsConfig.get("beeper.png"))
+      .scale(0.5, 0.5)
+      .with(BulletComponent(data.get("target") as Entity))
+      .buildAndAttach()
   }
 
 
