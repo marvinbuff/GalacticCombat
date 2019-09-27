ccc12d5c386c39165d36330f18f00a7fb7706ab7
marvinbuff <marvinbuff@hotmail.com>
2019-09-27 07:52:55 +0200

    Refactored Invader

--- src/main/kotlin/galacticCombat/invader/InvaderData.kt
+++ src/main/kotlin/galacticCombat/invader/InvaderData.kt
@@ -1,13 +1,18 @@
 package galacticCombat.invader
 
+import com.almasb.fxgl.dsl.FXGL
+import galacticCombat.GalacticCombatApp
+import javafx.geometry.Point2D
+
 data class InvaderData(
-    val texture: String,
-    val maxHealth: Double,
-    val baseSpeed: Speed,
-    val armour: Double,
-    val xp: Int,
-    val bounty: Int = 100,
-    val damage: Int = 1
+  val texture: String,
+  val maxHealth: Double,
+  val baseSpeed: Speed,
+  val armour: Double,
+  val xp: Int,
+  val bounty: Int = 100,
+  val damage: Int = 1,
+  val wayPoints: List<Point2D> = (FXGL.getApp() as GalacticCombatApp).waypoints
 ) {
   companion object {
     const val id = "InvaderData"
