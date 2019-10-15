883f4a6a0df0d54bf53534b7b5e28d0cb2739ed9
Marvin Buff <marvinbuff@hotmail.com>
2019-10-15 20:10:27 +0200

    Added waves to level files

--- src/main/kotlin/galacticCombat/level/LevelData.kt
+++ src/main/kotlin/galacticCombat/level/LevelData.kt
@@ -1,6 +1,7 @@
 package galacticCombat.level
 
 import galacticCombat.configs.LevelGameVars
+import galacticCombat.entities.invader.InvaderType
 import galacticCombat.utils.jsonSerializer
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.Serializable
@@ -9,6 +10,7 @@ import kotlinx.serialization.stringify
 
 @Serializable
 data class LevelData(
+    //move settings data into LevelSettingsData object
     val title: String,
     val initialGold: Int = 100,
     val trickleGold: Int = 5,
@@ -16,6 +18,7 @@ data class LevelData(
     val initialHealth: Int = 10,
     val initialExperience: Int = 0,
     val timePerWave: Double = 60.0,
+    val waves: List<Wave>,
     val paths: List<Path>
 ) {
   fun setGameVars() {
@@ -33,9 +36,16 @@ data class LevelData(
 @UnstableDefault
 @ImplicitReflectionSerializer
 fun main() {
-  println("Dummy Level File:")
+  println("Experiment Level File:")
   val path = Path("main_path", listOf(50 to 150, 150 to 350, 550 to 350, 130 to 120))
-  val data = LevelData("title", paths = listOf(path))
+  val invaders = listOf(
+      InvaderArgs(InvaderType.COMMON, 1),
+      InvaderArgs(InvaderType.ACCELERATED, 2),
+      InvaderArgs(InvaderType.REINFORCED, 3)
+  )
+  val spawnTimes = listOf(1, 2, 4).map { it.toDouble() }
+  val wave = Wave(spawnTimes.zip(invaders))
+  val data = LevelData("title", paths = listOf(path), waves = listOf(wave))
   val json = jsonSerializer.stringify(data)
   println(json)
 }
---
+++ src/main/kotlin/galacticCombat/level/Wave.kt
@@ -0,0 +1,17 @@
+package galacticCombat.level
+
+import galacticCombat.entities.invader.InvaderType
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Wave(
+    val invaders: List<Pair<Double, InvaderArgs>>,
+    val isBoss: Boolean = false
+)
+
+@Serializable
+data class InvaderArgs(
+    //todo add which path to spawn on
+    val type: InvaderType,
+    val level: Int
+)
