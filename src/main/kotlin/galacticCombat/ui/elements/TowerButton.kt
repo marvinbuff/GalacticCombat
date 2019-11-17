a7d47a3c3825cc08b76707a129e59a2301071ad9
Marvin Buff <marvinbuff@hotmail.com>
2019-11-17 08:54:17 +0100

    Added indication when tower is not placable

--- src/main/kotlin/galacticCombat/ui/elements/TowerButton.kt
+++ src/main/kotlin/galacticCombat/ui/elements/TowerButton.kt
@@ -1,11 +1,9 @@
 package galacticCombat.ui.elements
 
 import com.almasb.fxgl.dsl.FXGL
-import com.almasb.fxgl.dsl.getGameState
 import com.almasb.fxgl.dsl.getGameWorld
 import com.almasb.fxgl.dsl.getInput
 import com.almasb.fxgl.entity.SpawnData
-import galacticCombat.configs.LevelGameVars
 import galacticCombat.entities.PLACEHOLDER_SPAWN_ID
 import galacticCombat.entities.tower.TowerData
 import javafx.event.ActionEvent
@@ -16,16 +14,9 @@ class TowerButton(data: TowerData) : IconButton(getIcon(data), getHandler(data))
 
 private fun getIcon(data: TowerData) = FXGL.texture(data.texture)
 
-private fun getHandler(data: TowerData) = EventHandler<ActionEvent> { event ->
-  val goldId = LevelGameVars.GOLD.id
-
-  val gold = getGameState().getInt(goldId)
-  if (gold >= data.price) {
-    getGameState().increment(goldId, -data.price)
-    getGameWorld().spawn(
-      PLACEHOLDER_SPAWN_ID,
-        SpawnData(getInput().mousePositionWorld).put(TowerData.id, data)
-    )
-
-  }
+private fun getHandler(data: TowerData) = EventHandler<ActionEvent> {
+  getGameWorld().spawn(
+    PLACEHOLDER_SPAWN_ID,
+    SpawnData(getInput().mousePositionWorld).put(TowerData.id, data)
+  )
 }
