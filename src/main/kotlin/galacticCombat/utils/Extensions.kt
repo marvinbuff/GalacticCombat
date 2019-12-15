3a631c5bb176c06dbb9790593e506f632e3f0c3b
Marvin Buff <marvinbuff@hotmail.com>
2019-12-15 20:59:47 +0100

    Small Refactoring

--- src/main/kotlin/galacticCombat/utils/Extensions.kt
+++ src/main/kotlin/galacticCombat/utils/Extensions.kt
@@ -5,3 +5,6 @@ import javafx.geometry.Point2D
 fun Double.toPoint(): Point2D = Point2D(this, this)
 
 fun Pair<Int, Int>.toPoint(): Point2D = Point2D(first.toDouble(), second.toDouble())
+
+operator fun Point2D.component1(): Double = x
+operator fun Point2D.component2(): Double = y
