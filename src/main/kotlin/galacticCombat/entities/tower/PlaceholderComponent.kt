bcbb4a38857ac4d4d815e472aaafe472f7fbb899
Marvin Buff <marvinbuff@hotmail.com>
2020-03-16 09:36:57 +0100

    Implemented tower placeholder check Adjusted positions with anchor within world

--- src/main/kotlin/galacticCombat/entities/tower/PlaceholderComponent.kt
+++ src/main/kotlin/galacticCombat/entities/tower/PlaceholderComponent.kt
@@ -1,19 +1,17 @@
 package galacticCombat.entities.tower
 
-import com.almasb.fxgl.dsl.getAppHeight
-import com.almasb.fxgl.dsl.getAppWidth
 import com.almasb.fxgl.dsl.getInput
-import com.almasb.fxgl.entity.component.Component
+import galacticCombat.configs.GameConfig
 import galacticCombat.configs.LevelGameVars
+import galacticCombat.entities.generic.ClickableComponent
 import javafx.event.EventHandler
 import javafx.geometry.Point2D
-import javafx.geometry.Rectangle2D
 import javafx.scene.input.MouseButton
 import javafx.scene.input.MouseEvent
 
-class PlaceholderComponent(private val towerData: TowerData) : Component() {
+class PlaceholderComponent(private val towerData: TowerData) : ClickableComponent() {
 
-  private val onClick = EventHandler<MouseEvent> { event ->
+  override val onClick = EventHandler<MouseEvent> { event ->
     when (event.button) {
       MouseButton.PRIMARY   -> {
         if (isValidPosition()) {
@@ -26,15 +24,10 @@ class PlaceholderComponent(private val towerData: TowerData) : Component() {
       else                  -> {
       }
     }
-
-  }
-
-  override fun onAdded() {
-    entity.viewComponent.addEventHandler(MouseEvent.MOUSE_CLICKED, onClick)
   }
 
   override fun onUpdate(tpf: Double) {
-    entity.position = getInput().mousePositionWorld.subtract(TowerComponent.center)
+    entity.anchoredPosition = getInput().mousePositionWorld
     if (isValidPosition()) {
       entity.viewComponent.opacity = 1.0
     } else {
@@ -42,17 +35,22 @@ class PlaceholderComponent(private val towerData: TowerData) : Component() {
     }
   }
 
-  override fun onRemoved() {
-    entity.viewComponent.removeEventHandler(MouseEvent.MOUSE_CLICKED, onClick)
-  }
-
   private fun isValidPosition() = hasSufficientGold() && isAtValidTowerPosition()
 
   private fun hasSufficientGold(): Boolean = LevelGameVars.GOLD.get() >= towerData.price
 
   private fun isAtValidTowerPosition(): Boolean {
-    val mousePosition = getInput().mousePositionWorld
-    val worldBounds = Rectangle2D(0.0, 0.0, getAppWidth().toDouble(), getAppHeight().toDouble())
-    return worldBounds.contains(mousePosition)
+    val pos = entity.anchoredPosition
+    val anchor = entity.localAnchor
+
+    val corners = listOf(
+      pos.x - anchor.x to pos.y - anchor.y, //top left
+      pos.x + anchor.x to pos.y - anchor.y, //top right
+      pos.x - anchor.x to pos.y + anchor.y, //bottom left
+      pos.x + anchor.x to pos.y + anchor.y
+    ) //bottom right
+      .map { (x, y) -> Point2D(x, y) }
+
+    return corners.all { GameConfig.isPointInWorld(it) }
   }
 }
