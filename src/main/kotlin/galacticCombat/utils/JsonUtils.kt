e523b63a72f0660c59e7aa28c1c12483b5550bd1
Marvin Buff <marvinbuff@hotmail.com>
2020-03-24 19:35:22 +0100

    Fix weird issue

--- src/main/kotlin/galacticCombat/utils/JsonUtils.kt
+++ src/main/kotlin/galacticCombat/utils/JsonUtils.kt
@@ -3,17 +3,16 @@ package galacticCombat.utils
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.json.JsonConfiguration.Companion.Stable
 import kotlinx.serialization.parse
 import kotlinx.serialization.stringify
 import java.io.PrintWriter
 import java.net.URL
 
-@OptIn(ImplicitReflectionSerializer::class, UnstableDefault::class)
+@UseExperimental(ImplicitReflectionSerializer::class)
 inline fun <reified T : Any> loadJson(url: URL): T = jsonSerializer.parse(url.readText())
 
-@OptIn(ImplicitReflectionSerializer::class, UnstableDefault::class)
+@UseExperimental(ImplicitReflectionSerializer::class)
 inline fun <reified T : Any> writeJson(url: URL, dataObject: T) {
   val data = jsonSerializer.stringify(dataObject)
   val writer = PrintWriter(url.file)
