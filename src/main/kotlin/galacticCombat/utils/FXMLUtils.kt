6fd003f3c9afdc4ef2dcd83ed27297027f76f135
marvinbuff <marvinbuff@hotmail.com>
2020-06-01 10:15:59 +0200

    Add builder utils

--- src/main/kotlin/galacticCombat/utils/FXMLUtils.kt
+++ src/main/kotlin/galacticCombat/utils/FXMLUtils.kt
@@ -1,7 +1,9 @@
 package galacticCombat.utils
 
+import com.almasb.fxgl.dsl.EntityBuilder
 import com.almasb.fxgl.dsl.FXGL
 import com.almasb.fxgl.entity.GameWorld
+import com.almasb.fxgl.entity.component.Component
 import com.almasb.fxgl.logging.Logger
 import javafx.event.Event
 
@@ -14,4 +16,14 @@ fun Event.fire() {
 
 fun GameWorld.removeEntitiesByType(vararg types: Enum<*>) {
   removeEntities(getEntitiesByType(*types))
+}
+
+private fun EntityBuilder.conditionalWith(vararg comps: Component?) = this.also {
+  this.with(*comps.filterNotNull().toTypedArray())
+}
+
+private fun EntityBuilder.conditionalWith(comps: Component, condition: Boolean) = this.also {
+  if (condition) {
+    this.with(comps)
+  }
 }
